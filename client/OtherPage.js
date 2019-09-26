f0e6aff54f0d4b1f54844b663e440f89d9366e86
root <mashdas94@gmail.com>
2019-09-26 16:53:59 +0530

    Kaam Kr ja

---
+++ client/OtherPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+export default () => {
+
+	return (
+		
+		<div>
+			I'm some other page!                                                                           
+			
+			<Link to="/">Go back Home</Link>
+		
+		</div>
+
+	);
+};
